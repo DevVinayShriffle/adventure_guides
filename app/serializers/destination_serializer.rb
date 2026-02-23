325b977bcb2f49a8ee395857002effecad338b5e
DevVinayShriffle <vinayks@shriffle.com>
2026-02-23 20:16:12 +0530

    adding image url in serializers

--- app/serializers/destination_serializer.rb
+++ app/serializers/destination_serializer.rb
@@ -1,3 +1,13 @@
 class DestinationSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :location
+  include Rails.application.routes.url_helpers
+
+  attributes :id, :name, :description, :location, :image_urls
+
+  def image_urls
+    if object.images.attached?
+      object.images.map do |image|
+        rails_blob_url(image, only_path: false)
+      end
+    end
+  end
 end
